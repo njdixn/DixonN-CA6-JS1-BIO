c24541f61990b1d97d5d27ab8eba95f702fb0155
njdixn <110507018+njdixn@users.noreply.github.com>
2025-08-01 16:56:04 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// Hardcoded computer choiceas bear
+const computerChoice = 'Bear';
+
+// Prompt for players name
+let playerName = prompt('Welcome to Bear Hunter Ninja! Please enter your name to get started:');
+
+// Let's play message with players name
+let letsPlayMessage = playerName;
+    alert('Hi ' + letsPlayMessage + ' let\'s play!!');
+
+// Ask player who they want to be
+let playerChoice = prompt('Who are you: Bear, Ninja or Hunter?');
+
+// Prep player's results
+let resultMessagePlayer = playerName + ', you picked ' + playerChoice + '. ';
+
+// Prep computer's results
+let resultMessageComputer = 'The computer picked ' + computerChoice + '. ';
+
+// varaible for the results to be displayed
+let gameResult = '';
+
+// Result in HTML
+document.getElementById('gameResult').innerHTML =  resultMessagePlayer + '<br>' + resultMessageComputer;
